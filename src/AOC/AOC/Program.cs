001799d07f675cd40f944f2aca9f75130e10cc93
Erik Kvanli <0erikkvanli0@gmail.com>
2021-12-01 18:32:06 +0100

    Solve 1A

--- src/AOC/AOC/Program.cs
+++ src/AOC/AOC/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace AOC
 {
     class Program
     {
+        private static string InputLocation = "/Users/erikkvanli/Repos/AdventOfCode/Inputs";
+        private static string[] Lines;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Solve1();
+        }
+
+        static void Solve1()
+        {
+            Setup(1);
+            var input = ReadInts;
+            var prev = input[0];
+            var inc = 0;
+            for(var i = 0; i< input.Length; i++)
+            {
+                if (input[i] > prev) inc++;
+                prev = input[i];
+            }
+            Console.WriteLine(inc);
+        }
+
+        static int[] ReadInts => Lines.Select(int.Parse).ToArray();
+
+        static void Setup(int number)
+        {
+            Lines = File.ReadAllLines(Path.Combine(InputLocation, number + ".txt"));
         }
     }
 }
